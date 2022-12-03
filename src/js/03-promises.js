1d786f5652d5cf202f8e0e15387d5038621ea40b
DemouserNG <108471967+DemouserNG@users.noreply.github.com>
2022-12-03 18:15:04 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,9 +1,44 @@
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmitButton);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    })
+  }, delay);
+
 }
 
+function onSubmitButton(e) {
+  e.preventDefault();
+
+  let delayValue = Number(e.target.elements.delay.value);
+  let stepValue = Number(e.target.elements.step.value);
+  let amountValue = Number(e.target.elements.amount.value);
+
+  for (let i = 1; i <= amountValue; i += 1) {
+    createPromise(i, delayValue).then(onSuccess).catch(onError);
+    delayValue += stepValue;
+  }
+
+}
+
+
+function onSuccess({ position, delay }) {
+  console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+
+};
+function onError({ position, delay }) {
+  console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+}
